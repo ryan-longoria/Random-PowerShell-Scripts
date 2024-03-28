7dd45f2c30eb4f1e9aff2f0f86c6849e20ec0915
Ryan Longoria (Kyro) <89551302+ryan-longoria@users.noreply.github.com>
2024-03-28 16:22:36 -0500

    Add files via upload

---
+++ OutlookCachedExchangeMode.ps1
@@ -0,0 +1,2 @@
+# For Windows. Path may need to be edited depending on Outlook or Windows version
+Set-ItemProperty "HKCU:\software\policies\microsoft\office\16.0\outlook\cachedmode" -Name enable -Value 1
